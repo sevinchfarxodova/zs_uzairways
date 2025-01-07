5dbf44fdec670fe713ae255c8f587c71995f590e
sevinchfarxodova <150798606+sevinchfarxodova@users.noreply.github.com>
2025-01-07 15:25:14 +0500

    auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-   return Placeholder();
+   return Scaffold(
+     appBar: AppBar(
+
+     ),
+   );
   }
 }
 
