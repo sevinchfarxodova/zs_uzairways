1895a799900becbdec97cefba0dc9ce2c9939225
zeienv <zaynabxonergasheva05@icloud.com>
2025-01-13 01:12:58 -0800

    new

--- lib/myApp.dart
+++ lib/myApp.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: RouteNames.account,
+      initialRoute: RouteNames.searchFlights,
       onGenerateRoute: AppRoute(context: context).onGenerateRoute,
     );
   }
